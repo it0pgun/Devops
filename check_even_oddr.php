2b0dba47f10d8a9448e08f3572ece99a3e30e7ad
topgun <lyashko.egor@gmail.com>
2024-02-25 13:44:05 +0200

    [topgun] Add check_even_odd

---
+++ check_even_oddr.php
@@ -0,0 +1,13 @@
+<?php
+function checkEvenOdd($number) {
+    if ($number % 2 == 0) {
+        return "Число $number є парним.";
+    } else {
+        return "Число $number є непарним.";
+    }
+}
+
+// Тестування функції
+$testNumber = 4;
+echo checkEvenOdd($testNumber);
+?>
